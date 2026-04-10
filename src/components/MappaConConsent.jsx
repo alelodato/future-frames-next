c3b0cca60d1448a4241a77cfc4a5110764a689b9
Alessio <alss.ldt@gmail.comm>
2026-04-10 10:36:47 +0200

    rimosso grayscal mappa per mobile

--- src/components/MappaConConsent.jsx
+++ src/components/MappaConConsent.jsx
@@ -11,7 +11,7 @@ export default function MappaConConsent() {
                 title="Mappa Pomezia"
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-                className="h-full w-full min-h-[300px] border-0 grayscale contrast-110 opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100"
+                className="h-full w-full min-h-[300px] border-0 lg:grayscale contrast-110 opacity-60 transition duration-500 hover:grayscale-0 hover:opacity-100"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7469.59805370534!2d12.494142676655008!3d41.669457171703194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132599a4cfbcba4b%3A0xdee4c5c2d3b0f27b!2s00071%20Pomezia%20RM!5e0!3m2!1sit!2sit!4v1733589000000!5m2!1sit!2si"
             />
         );
